33798c43f2b3651fc0c060b4419b6bfd85acad89
dli7319 <dli7319@gmail.com>
2020-05-24 10:46:29 -0400

    Change to using tokens

--- README.md
+++ README.md
@@ -1,18 +1,40 @@
 # ddns-cloudflare-javascript
-Another DDNS Updater for Cloudflare written in Javascript.  
+Another DDNS Updater for Cloudflare written in Javascript.
 This will scan for all "A" records and update their IPs.
 
 ------------
 
-Requirements
-* Node.js and npm
-
-Usage:
+## Usage:
 * Clone or download the repo
 * Run `npm install` to install all necessary dependencies.
-* Copy `parameters_example.json` to `parameters.json`
+* Copy `parameters.json.example` to `parameters.json`
 * Fill in `parameters.json` with information from your Cloudflare Account. You can remove the zones array to scan for all zones.
+ * To use a token, create a token with Zone.Zone and Zone.DNS edit permissions at https://dash.cloudflare.com/profile/api-tokens.
+ * To use an API Key, you must fill in `API_KEY` and `EMAIL` in `parameters.json`.
+   See `parameters.json.example2`.
 * Run `node main.js` to update records on Cloudflare.
-* Make a cron job to run this every so often with `crontab -e`  
+* Make a cron job to run this every so often with `crontab -e`
 Appending the following to update every 15 minutes
 `*/15 * * * * node /full/path/to/ddns-cloudflare-javascript/main.js /full/path/to/ddns-cloudflare-javascript/parameters.json >/dev/null 2>&1`
+
+### Blacklist:
+To create a blacklist of records, add an `EXCLUSIONS` to `parameters.json`:
+```
+{
+  ...
+  "EXCLUSIONS": [
+    "store.example.org"
+  ]
+}
+```
+
+### Whitelist:
+To create a whitelist of records, add an `INCLUSIONS` key to `parameters.json`:
+```
+{
+  ...
+  "INCLUSIONS": [
+    "store.example.org"
+  ]
+}
+```
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const fetch = require('node-fetch');
+const fs = require("fs");
+const fetch = require("node-fetch");
 
 const CONSTANTS = {
   cloudflare_endpoint: "https://api.cloudflare.com/client/v4/",
@@ -19,7 +19,8 @@ function main() {
       currentIP = values[1].ip;
       console.log("Retrieved Current IP:", "--" + currentIP + "--");
       return;
-    }).then(() => {
+    })
+    .then(() => {
       // Get Zones from file or from Cloudflare API
       if (parameters.ZONES) {
         let zones = parameters.ZONES;
@@ -33,7 +34,8 @@ function main() {
       }
       console.log("Retrieving Current List of Zones from Cloudflare");
       return listZones(parameters);
-    }).then(zones => {
+    })
+    .then(zones => {
       // Get DNS records from Cloudflare API
       if (!zones.success) {
         console.error("Could not read zones");
@@ -44,7 +46,8 @@ function main() {
         promises.push(listDNSRecords(parameters, zone.id));
       });
       return Promise.all(promises);
-    }).then(zones => {
+    })
+    .then(zones => {
       // Collect DNS Records to Update
       let recordsArr = [];
       zones.forEach(records => {
@@ -52,8 +55,13 @@ function main() {
           console.error("FAILED TO RETRIEVE RECORDS FOR", records);
         } else {
           records.result.forEach(record => {
-            if (record.type === 'A') {
-              if (parameters.EXCLUSIONS && parameters.EXCLUSIONS.includes(record.name)) {
+            if (record.type === "A") {
+              if (
+                (parameters.EXCLUSIONS &&
+                  parameters.EXCLUSIONS.includes(record.name)) ||
+                (parameters.INCLUSIONS &&
+                  !parameters.INCLUSIONS.includes(record.name))
+              ) {
                 console.log("Skipping:", record.name);
               } else if (record.content === currentIP) {
                 console.log("Record Already Up To Date:", record.name);
@@ -65,21 +73,27 @@ function main() {
         }
       });
       return recordsArr;
-    }).then(recordsArr => {
+    })
+    .then(recordsArr => {
       // Update DNS Records
       recordsArr.forEach(record => {
         let update = {
-          "type": record.type,
-          "name": record.name,
-          "content": currentIP,
-          "ttl": record.ttl,
-          "proxied": record.proxied
+          type: record.type,
+          name: record.name,
+          content: currentIP,
+          ttl: record.ttl,
+          proxied: record.proxied
         };
         updateRecord(parameters, record, update).then(response => {
           if (response.success) {
             console.log("Updated Record For:", response.result.name);
           } else {
-            console.log("Record Update Failed for", record, "with Error", response.name);
+            console.log(
+              "Record Update Failed for",
+              record,
+              "with Error",
+              response.name
+            );
           }
         });
       });
@@ -88,7 +102,8 @@ function main() {
 
 function readParameters() {
   return new Promise(resolve => {
-    let filePath = process.argv.length >= 3 ? process.argv[2] : "parameters.json";
+    let filePath =
+      process.argv.length >= 3 ? process.argv[2] : "parameters.json";
     fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         console.error("Cannot read parameters.json", err);
@@ -112,14 +127,11 @@ function getCurrentIP() {
 function listZones(parameters) {
   return new Promise(resolve => {
     fetch(CONSTANTS.cloudflare_endpoint + "zones", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "X-Auth-Email": parameters.EMAIL,
-          "X-Auth-Key": parameters.API_KEY,
-          "Content-Type": "application/json"
-        }
-      }).then(response => response.json())
+      method: "GET",
+      mode: "cors",
+      headers: getHeaders(parameters)
+    })
+      .then(response => response.json())
       .then(data => {
         resolve(data);
       });
@@ -129,14 +141,11 @@ function listZones(parameters) {
 function listDNSRecords(parameters, zoneId) {
   return new Promise(resolve => {
     fetch(CONSTANTS.cloudflare_endpoint + "zones/" + zoneId + "/dns_records", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "X-Auth-Email": parameters.EMAIL,
-          "X-Auth-Key": parameters.API_KEY,
-          "Content-Type": "application/json"
-        }
-      }).then(response => response.json())
+      method: "GET",
+      mode: "cors",
+      headers: getHeaders(parameters)
+    })
+      .then(response => response.json())
       .then(data => {
         resolve(data);
       });
@@ -145,18 +154,41 @@ function listDNSRecords(parameters, zoneId) {
 
 function updateRecord(parameters, record, update) {
   return new Promise(resolve => {
-    fetch(CONSTANTS.cloudflare_endpoint + "zones/" + record.zone_id + "/dns_records/" + record.id, {
+    fetch(
+      CONSTANTS.cloudflare_endpoint +
+        "zones/" +
+        record.zone_id +
+        "/dns_records/" +
+        record.id,
+      {
         method: "PUT",
         mode: "cors",
-        headers: {
-          "X-Auth-Email": parameters.EMAIL,
-          "X-Auth-Key": parameters.API_KEY,
-          "Content-Type": "application/json"
-        },
+        headers: getHeaders(parameters),
         body: JSON.stringify(update)
-      }).then(response => response.json())
+      }
+    )
+      .then(response => response.json())
       .then(data => {
         resolve(data);
       });
   });
+}
+
+function getHeaders(parameters, keys = null) {
+  let headers = {
+    "Content-Type": "application/json"
+  };
+  if (parameters.TOKEN) {
+    headers.Authorization = "Bearer " + parameters.TOKEN;
+  } else if (parameters.API_KEY) {
+    headers["X-Auth-Email"] = parameters.EMAIL;
+    headers["X-Auth-Key"] = parameters.API_KEY;
+  } else {
+    console.error("No authentication provided in parameters.json");
+    process.exit(1);
+  }
+  if (keys) {
+    Object.assign(headers, keys);
+  }
+  return headers;
 }
