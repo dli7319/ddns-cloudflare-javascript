c98c7d7988f79543f6832f2b652c9196a266d41e
David Li <david@davidl.me>
2022-02-06 00:43:10 +0000

    Use tee instead of bash redirect

--- update_records.sh
+++ update_records.sh
@@ -6,4 +6,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-npm start >update_records.log 2>&1
+npm start | tee update_records.log
