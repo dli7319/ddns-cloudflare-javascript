940108435278622f56c5b3f49519c1f5ab52dcfa
David Li <david@davidl.me>
2023-09-24 03:48:22 +0000

    Fix run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-cd "${DIR}/.." || exit 1
+cd "${DIR}" || exit 1
 
 image=$(docker build -q .)
 docker run -v "$(pwd)/parameters.yaml:/home/node/parameters.yaml" --net host "$image"
